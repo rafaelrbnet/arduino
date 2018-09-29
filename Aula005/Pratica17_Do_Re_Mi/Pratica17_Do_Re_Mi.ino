2e223c65b1e8ccae04a376ec2ee2d56d2005855a
Rafael Baena Neto <rafael.baena@gmail.com>
2018-09-29 10:12:19 -0300

    Exercício 17 da aula 5

---
+++ Aula005/Pratica17_Do_Re_Mi/Pratica17_Do_Re_Mi.ino
@@ -0,0 +1,54 @@
+void setup() {
+pinMode(10,OUTPUT);} //Pino do buzzer
+
+void loop(){
+    delay(2000);
+    tone(10,262,200); //DO
+    delay(220);
+    tone(10,294,300); //RE
+    delay(220);
+    tone(10,330,300); //MI
+    delay(220);
+    tone(10,349,300); //FA
+    delay(330);
+    tone(10,349,300); //FA
+    delay(330);
+    tone(10,349,300); //FA
+    delay(330);
+    tone(10,262,100); //DO
+    delay(220);
+    tone(10,294,300); //RE
+    delay(220);   
+    tone(10,262,100); //DO
+    delay(220);
+    tone(10,294,300); //RE
+    delay(330);
+    tone(10,294,300); //RE
+    delay(330);
+    tone(10,294,300); //RE
+    delay(330);
+    tone(10,262,200); //DO
+    delay(220);
+    tone(10,392,200); //SOL
+    delay(220);
+    tone(10,349,200); //FA
+    delay(220);
+    tone(10,330,300); //MI
+    delay(330);
+    tone(10,330,300); //MI
+    delay(330);
+    tone(10,330,300); //MI
+    delay(330);
+    tone(10,262,200); //DO
+    delay(220);
+    tone(10,294,300); //RE
+    delay(220);
+    tone(10,330,300); //MI
+    delay(220);
+    tone(10,349,300); //FA
+    delay(330);
+    tone(10,349,300); //FA
+    delay(330);
+    tone(10,349,300); //FA
+    delay(330);
+}
